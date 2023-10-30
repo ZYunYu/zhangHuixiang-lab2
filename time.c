77c0e9fd24ccfa68dab4cd6b6071e3c53572d0bd
Huixiang Zhang <z.yun.yu@outlook.com>
2023-10-29 23:29:12 -0400

    add elapsed_time

--- time.c
+++ time.c
@@ -2,6 +2,8 @@
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
-    
-    return 0.0;
+    double seconds = end_time->tv_sec - start_time->tv_sec;
+    double microseconds = (end_time->tv_usec - start_time->tv_usec)/1000000.0;
+
+    return seconds + microseconds;
 }
